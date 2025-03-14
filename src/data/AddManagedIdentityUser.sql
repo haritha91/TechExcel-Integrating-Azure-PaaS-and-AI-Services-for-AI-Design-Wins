bf72bc5d452cfcd336a6845a460d7ba2d5005aab
Haritha Thilakarathne <harithalht@gmail.com>
2025-03-14 13:47:09 +1100

    add square brackets

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER id-zbrtrmu3tarwe FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER id-zbrtrmu3tarwe;
-ALTER ROLE db_datawriter ADD MEMBER id-zbrtrmu3tarwe;
+CREATE USER [id-zbrtrmu3tarwe] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [id-zbrtrmu3tarwe];
+ALTER ROLE db_datawriter ADD MEMBER [id-zbrtrmu3tarwe];
 
